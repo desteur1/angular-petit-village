f6980b8a7d3691d3d47438314be465d7995ccd3a
Piepench Fall Desteur <shmurdanoah@gmail.com>
2024-12-15 09:18:15 +0100

    end of notfoundpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { NotFoundpageComponent } from './not-foundpage/not-foundpage.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
+  { path: '**', component: NotFoundpageComponent },
 ];
---
+++ src/app/not-foundpage/not-foundpage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-not-foundpage',
+  imports: [RouterLink, RouterModule],
+  templateUrl: './not-foundpage.component.html',
+  styleUrl: './not-foundpage.component.css',
+})
+export class NotFoundpageComponent {}
